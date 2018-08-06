0115a3e7ba9d68893c378d393ebc510a7186521d
vsh1ft <kastytis.ch@gmail.com>
2018-08-06 13:22:47 +0300

    BA-443: code moved from logout service to workLogService

--- src/main/kotlin/lt/boldadmin/ntropy/LogoutScheduler.kt
+++ src/main/kotlin/lt/boldadmin/ntropy/LogoutScheduler.kt
@@ -1,14 +1,14 @@
 package lt.boldadmin.ntropy
 
-import lt.boldadmin.nexus.service.LogoutService
+import lt.boldadmin.nexus.service.worklog.WorkLogService
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 
 @Component
-class LogoutScheduler(private val logoutService: LogoutService) {
+class LogoutScheduler(private val workLogService: WorkLogService) {
 
     @Scheduled(cron = "*/3 * * * * *")
     fun schedule() {
-        logoutService.logout()
+        workLogService.logout()
     }
 }
--- src/test/kotlin/lt/boldadmin/ntropy/test/unit/LogoutSchedulerTest.kt
+++ src/test/kotlin/lt/boldadmin/ntropy/test/unit/LogoutSchedulerTest.kt
@@ -2,7 +2,7 @@ package lt.boldadmin.ntropy.test.unit
 
 import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.verify
-import lt.boldadmin.nexus.service.LogoutService
+import lt.boldadmin.nexus.service.worklog.WorkLogService
 import lt.boldadmin.ntropy.LogoutScheduler
 import org.junit.Test
 
@@ -10,11 +10,11 @@ class LogoutSchedulerTest {
 
     @Test
     fun `Schedules logout for collaborators`() {
-        val logoutServiceSpy = mock<LogoutService>()
+        val workLogServiceSpy = mock<WorkLogService>()
 
-        LogoutScheduler(logoutServiceSpy).schedule()
+        LogoutScheduler(workLogServiceSpy).schedule()
 
-        verify(logoutServiceSpy).logout()
+        verify(workLogServiceSpy).logout()
     }
 
 }

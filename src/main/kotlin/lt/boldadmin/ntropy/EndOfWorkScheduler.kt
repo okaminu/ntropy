76ef4fa790ef41502de3b274f669f77d705b79cb
Okaminu <aurimasdgt@gmail.com>
2018-10-09 17:55:51 +0300

    Revert "BA-442: removed ability for WorkLogService to act as a facade for other worklog services"
    This reverts commit 885dd42

--- src/main/kotlin/lt/boldadmin/ntropy/EndOfWorkScheduler.kt
+++ src/main/kotlin/lt/boldadmin/ntropy/EndOfWorkScheduler.kt
@@ -1,15 +1,15 @@
 package lt.boldadmin.ntropy
 
-import lt.boldadmin.nexus.service.worklog.status.WorkLogStartEndService
+import lt.boldadmin.nexus.service.worklog.WorkLogService
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 
 @Component
-class EndOfWorkScheduler(private val workLogStartEndService: WorkLogStartEndService) {
+class EndOfWorkScheduler(private val workLogService: WorkLogService) {
 
     @Scheduled(cron = "0 */10 * * * *")
     fun schedule() {
-        workLogStartEndService.endAllStartedWorkWhereWorkTimeEnded()
+        workLogService.endAllStartedWorkWhereWorkTimeEnded()
     }
 
 }
--- src/test/kotlin/lt/boldadmin/ntropy/test/unit/EndOfWorkSchedulerTest.kt
+++ src/test/kotlin/lt/boldadmin/ntropy/test/unit/EndOfWorkSchedulerTest.kt
@@ -2,7 +2,7 @@ package lt.boldadmin.ntropy.test.unit
 
 import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.verify
-import lt.boldadmin.nexus.service.worklog.status.WorkLogStartEndService
+import lt.boldadmin.nexus.service.worklog.WorkLogService
 import lt.boldadmin.ntropy.EndOfWorkScheduler
 import org.junit.Test
 
@@ -10,11 +10,11 @@ class EndOfWorkSchedulerTest {
 
     @Test
     fun `Schedules an end of work for collaborators`() {
-        val workLogStartEndServiceSpy = mock<WorkLogStartEndService>()
+        val workLogServiceSpy = mock<WorkLogService>()
 
-        EndOfWorkScheduler(workLogStartEndServiceSpy).schedule()
+        EndOfWorkScheduler(workLogServiceSpy).schedule()
 
-        verify(workLogStartEndServiceSpy).endAllStartedWorkWhereWorkTimeEnded()
+        verify(workLogServiceSpy).endAllStartedWorkWhereWorkTimeEnded()
     }
 
 }

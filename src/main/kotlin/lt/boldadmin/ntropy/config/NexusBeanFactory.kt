2d045626d7a61585273e57f29786bf852cedae32
vsh1ft <kastytis.ch@gmail.com>
2018-12-17 00:31:04 +0200

    BA-764: circular dependency issue solved

---
+++ src/main/kotlin/lt/boldadmin/ntropy/config/NexusBeanFactory.kt
@@ -0,0 +1,26 @@
+package lt.boldadmin.ntropy.config
+
+import lt.boldadmin.nexus.api.service.worklog.CollaboratorUpdateListener
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.support.GenericApplicationContext
+
+@Configuration
+open class NexusBeanFactory(private var context: GenericApplicationContext) {
+
+    @Bean
+    open fun createCollaboratorUpdateListenerProvider() =
+        {
+            mutableMapOf(
+                "workTime.startOfDayInMinutes" to context.getBean<CollaboratorUpdateListener>(
+                    "collaboratorWorkTimeStartUpdateListener",
+                    CollaboratorUpdateListener::class.java
+                ),
+                "workTime.endOfDayInMinutes" to context.getBean<CollaboratorUpdateListener>(
+                    "collaboratorWorkTimeEndUpdateListener",
+                    CollaboratorUpdateListener::class.java
+                )
+            )
+        }
+
+}

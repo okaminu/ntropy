7375f62f4a7dd8e333ddea93d4934280e92611ed
Okaminu <aurimasdgt@gmail.com>
2018-12-18 17:22:59 +0200

    BA-742: nexus backend client adapter init

--- src/main/kotlin/lt/boldadmin/ntropy/Application.kt
+++ src/main/kotlin/lt/boldadmin/ntropy/Application.kt
@@ -2,14 +2,20 @@ package lt.boldadmin.ntropy
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.ImportResource
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.FilterType
 import org.springframework.scheduling.annotation.EnableScheduling
 
 @EnableAutoConfiguration
-@ImportResource("classpath:context/context.xml")
 @EnableScheduling
+@ComponentScan(
+        basePackages = ["lt.boldadmin.ntropy", "lt.boldadmin.nexus.backend.adapter.client"],
+        includeFilters = [ComponentScan.Filter(
+                type = FilterType.REGEX,
+                pattern = ["lt.boldadmin.nexus.backend.adapter.client.*"]
+        )]
+)
 class Application
-
 fun main(args: Array<String>) {
     runApplication<Application>(*args)
 }

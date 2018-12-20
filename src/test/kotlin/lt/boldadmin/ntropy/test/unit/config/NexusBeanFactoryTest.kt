fec051d8c008f814f40aa6b346631fcc501c3991
Okaminu <aurimasdgt@gmail.com>
2018-12-20 15:57:03 +0200

    BA-764: code review fixes

--- src/test/kotlin/lt/boldadmin/ntropy/test/unit/config/NexusBeanFactoryTest.kt
+++ src/test/kotlin/lt/boldadmin/ntropy/test/unit/config/NexusBeanFactoryTest.kt
@@ -1,7 +1,10 @@
 package lt.boldadmin.ntropy.test.unit.config
 
-import com.nhaarman.mockito_kotlin.*
-import lt.boldadmin.nexus.api.service.worklog.CollaboratorUpdateListener
+import com.nhaarman.mockito_kotlin.any
+import com.nhaarman.mockito_kotlin.doReturn
+import com.nhaarman.mockito_kotlin.eq
+import com.nhaarman.mockito_kotlin.mock
+import lt.boldadmin.nexus.api.service.worklog.CollaboratorUpdateSubscriber
 import lt.boldadmin.ntropy.config.NexusBeanFactory
 import org.junit.Test
 import org.springframework.context.support.GenericApplicationContext
@@ -10,18 +13,18 @@ import kotlin.test.assertEquals
 class NexusBeanFactoryTest {
 
     @Test
-    fun `Provides Collaborator update listeners map`() {
+    fun `Provides Collaborator update subscribers map`() {
         val contextStub: GenericApplicationContext = mock()
-        val startTimeUpdateDummy: CollaboratorUpdateListener = mock()
-        val endTimeUpdateDummy: CollaboratorUpdateListener = mock()
+        val startTimeUpdateDummy: CollaboratorUpdateSubscriber = mock()
+        val endTimeUpdateDummy: CollaboratorUpdateSubscriber = mock()
         val expectedListenersMap = mutableMapOf(
             "workTime.startOfDayInMinutes" to startTimeUpdateDummy,
             "workTime.endOfDayInMinutes" to endTimeUpdateDummy
         )
         doReturn(startTimeUpdateDummy, endTimeUpdateDummy).`when`(contextStub)
-            .getBean(any<String>(), eq(CollaboratorUpdateListener::class.java))
+            .getBean(any<String>(), eq(CollaboratorUpdateSubscriber::class.java))
 
-        val actualListenersMap = NexusBeanFactory(contextStub).createCollaboratorUpdateListenerProvider().invoke()
+        val actualListenersMap = NexusBeanFactory(contextStub).createCollaboratorUpdateSubscribersProvider().invoke()
 
         assertEquals(expectedListenersMap, actualListenersMap)
     }

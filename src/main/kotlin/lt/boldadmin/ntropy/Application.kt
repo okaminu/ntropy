f0b124a10580d88e6b1be7b5255c1effb347c520
Okaminu <aurimasdgt@gmail.com>
2019-01-30 17:25:15 +0200

    BA-742: backend loads nexus-backend-client dependencies, plugin takes backend connection address from environment

--- src/main/kotlin/lt/boldadmin/ntropy/Application.kt
+++ src/main/kotlin/lt/boldadmin/ntropy/Application.kt
@@ -1,21 +1,13 @@
-package lt.boldadmin.ntropy
-
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.ComponentScan
-import org.springframework.context.annotation.FilterType
+import org.springframework.context.annotation.ImportResource
 import org.springframework.scheduling.annotation.EnableScheduling
 
 @EnableAutoConfiguration
+@ImportResource("classpath:context/context.xml")
 @EnableScheduling
-@ComponentScan(
-        basePackages = ["lt.boldadmin.ntropy", "lt.boldadmin.nexus.plugin.backendclient"],
-        includeFilters = [ComponentScan.Filter(
-                type = FilterType.REGEX,
-                pattern = ["lt.boldadmin.nexus.plugin.backendclient.*"]
-        )]
-)
 class Application
+
 fun main(args: Array<String>) {
     runApplication<Application>(*args)
 }

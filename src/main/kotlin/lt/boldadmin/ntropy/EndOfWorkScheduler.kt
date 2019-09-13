1881f9a71b8283fdc778f824906bd32a35a07040
Okaminu <aurimasdgt@gmail.com>
2019-09-13 17:32:28 +0300

    BA-818: renamed WorklogStartEndService to WorklogStatusService and subscribed to collaborator message event

--- src/main/kotlin/lt/boldadmin/ntropy/EndOfWorkScheduler.kt
+++ src/main/kotlin/lt/boldadmin/ntropy/EndOfWorkScheduler.kt
@@ -1,15 +1,15 @@
 package lt.boldadmin.ntropy
 
-import lt.boldadmin.nexus.api.service.worklog.WorklogStartEndService
+import lt.boldadmin.nexus.api.service.worklog.WorklogStatusService
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 
 @Component
-class EndOfWorkScheduler(private val worklogStartEndService: WorklogStartEndService) {
+class EndOfWorkScheduler(private val worklogStatusService: WorklogStatusService) {
 
     @Scheduled(cron = "0 */10 * * * *")
     fun schedule() {
-        worklogStartEndService.endAllStartedWorkWhereWorkTimeEnded()
+        worklogStatusService.endAllStartedWorkWhereWorkTimeEnded()
     }
 
 }
--- src/test/kotlin/lt/boldadmin/ntropy/test/unit/EndOfWorkSchedulerTest.kt
+++ src/test/kotlin/lt/boldadmin/ntropy/test/unit/EndOfWorkSchedulerTest.kt
@@ -2,7 +2,7 @@ package lt.boldadmin.ntropy.test.unit
 
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.verify
-import lt.boldadmin.nexus.api.service.worklog.WorklogStartEndService
+import lt.boldadmin.nexus.api.service.worklog.WorklogStatusService
 import lt.boldadmin.ntropy.EndOfWorkScheduler
 import org.junit.jupiter.api.Test
 
@@ -10,11 +10,11 @@ class EndOfWorkSchedulerTest {
 
     @Test
     fun `Schedules an end of work for collaborators`() {
-        val worklogStartEndServiceSpy: WorklogStartEndService = mock()
+        val worklogStatusServiceSpy: WorklogStatusService = mock()
 
-        EndOfWorkScheduler(worklogStartEndServiceSpy).schedule()
+        EndOfWorkScheduler(worklogStatusServiceSpy).schedule()
 
-        verify(worklogStartEndServiceSpy).endAllStartedWorkWhereWorkTimeEnded()
+        verify(worklogStatusServiceSpy).endAllStartedWorkWhereWorkTimeEnded()
     }
 
 }

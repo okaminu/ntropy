3b94fb14d416810b637c92adc91423b48756ad71
vsh1ft <kastytis.ch@gmail.com>
2019-09-27 14:32:34 +0300

    BA-734: added missing acceptance test step, cleanup

--- src/main/kotlin/lt/boldadmin/ntropy/EndOfWorkScheduler.kt
+++ src/main/kotlin/lt/boldadmin/ntropy/EndOfWorkScheduler.kt
@@ -1,11 +1,11 @@
 package lt.boldadmin.ntropy
 
-import lt.boldadmin.nexus.api.service.worklog.status.WorklogsEndService
+import lt.boldadmin.nexus.api.service.worklog.status.WorklogOvertimeService
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 
 @Component
-class EndOfWorkScheduler(private val service: WorklogsEndService) {
+class EndOfWorkScheduler(private val service: WorklogOvertimeService) {
 
     @Scheduled(cron = "0 */10 * * * *")
     fun schedule() {
--- src/test/kotlin/lt/boldadmin/ntropy/test/unit/EndOfWorkSchedulerTest.kt
+++ src/test/kotlin/lt/boldadmin/ntropy/test/unit/EndOfWorkSchedulerTest.kt
@@ -2,8 +2,7 @@ package lt.boldadmin.ntropy.test.unit
 
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.verify
-import lt.boldadmin.nexus.api.service.worklog.status.WorklogStartEndService
-import lt.boldadmin.nexus.api.service.worklog.status.WorklogsEndService
+import lt.boldadmin.nexus.api.service.worklog.status.WorklogOvertimeService
 import lt.boldadmin.ntropy.EndOfWorkScheduler
 import org.junit.jupiter.api.Test
 
@@ -11,7 +10,7 @@ class EndOfWorkSchedulerTest {
 
     @Test
     fun `Schedules an end of work for collaborators`() {
-        val serviceSpy: WorklogsEndService = mock()
+        val serviceSpy: WorklogOvertimeService = mock()
 
         EndOfWorkScheduler(serviceSpy).schedule()
 
